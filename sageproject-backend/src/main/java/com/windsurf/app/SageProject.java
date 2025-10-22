592264721f85924d84029fdcbc53d90df9cb1825
rohithverma04 <rohithverma.kadari04@gmail.com>
2025-10-22 14:55:45 -0500

    Updated Naming

--- sageproject-backend/src/main/java/com/windsurf/app/SageProject.java
+++ sageproject-backend/src/main/java/com/windsurf/app/SageProject.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class WindsurfApplication {
+public class SageProject {
     public static void main(String[] args) {
-        SpringApplication.run(WindsurfApplication.class, args);
+        SpringApplication.run(SageProject.class, args);
     }
 }
